3f4a833c0031569b8f387767cc3ebc35493dcd18
Dima Portenko <troublesohard@gmail.com>
2025-05-04 13:50:56 +0300

    fix image appearance

--- app/components/floating-profile/index.tsx
+++ app/components/floating-profile/index.tsx
@@ -1,5 +1,13 @@
 import "./styles.css";
 
 export function FloatingProfile() {
-  return <div className="floating-border"></div>;
+  return (
+    <div className="floating-border border-8 border-black/10">
+      <img
+        src="/home/profile.jpeg"
+        alt="Dima Portenko"
+        className="w-[250px] h-[250px] object-cover"
+      />
+    </div>
+  );
 }
