f9964167e626b155e246c5fe5fb3a88391e370d6
Dima Portenko <troublesohard@gmail.com>
2025-05-06 23:04:30 +0300

    basic blog mdx setup

--- app/routes.ts
+++ app/routes.ts
@@ -4,9 +4,26 @@ import {
   layout,
   route,
 } from "@react-router/dev/routes"
+import { flatRoutes } from "@react-router/fs-routes"
+
+const routes = await flatRoutes({ rootDirectory: "routes/blog/" })
+
+// Convert blog folder files to routes
+const blogRoutes = routes.map((route) => {
+  return {
+    id: route.id,
+    file: route.file,
+    path: `blog/${route.path}`,
+    index: undefined,
+    caseSensitive: undefined,
+  }
+})
 
 export default [
-  layout("routes/main-layout.tsx", [index("routes/home.tsx")]),
+  layout("routes/main-layout.tsx", [
+    index("routes/home.tsx"),
+    ...blogRoutes,
+  ]),
 
   route("action/set-theme", "routes/action/set-theme.ts"),
 ] satisfies RouteConfig
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import { reactRouter } from "@react-router/dev/vite"
 import { cloudflare } from "@cloudflare/vite-plugin"
 import tailwindcss from "@tailwindcss/vite"
+import mdx from "@mdx-js/rollup"
 import { defineConfig } from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
   plugins: [
+    {
+      enforce: "pre",
+      ...mdx(),
+    },
     cloudflare({ viteEnvironment: { name: "ssr" } }),
     tailwindcss(),
     reactRouter(),
